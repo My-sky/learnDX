fafced3d7010e23283b237e8a750135df7b9299f
lihao <lihaohappy521@126.com>
2018-04-08 23:55:00 +0800

    add color createbuffer code

--- Chaper6/Chaper6/boxDemo.cpp
+++ Chaper6/Chaper6/boxDemo.cpp
@@ -157,5 +157,52 @@ void Box::CreateGeometryBuffers()
 	};
 
 	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * 8;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = vertex;
+	HR(m_pd3dDevice->CreateBuffer(&vbd, &vinitData, &mBoxVB));
 
+	//create the index buffer
+	UINT indices[] =
+	{
+		//front face
+		0,1,2,
+		0,2,3,
+
+		//back face
+		4,6,5,
+		4,7,6,
+
+		//left face
+		4,5,1,
+		4,1,0,
+
+		//right face
+		3,2,6,
+		3,6,7,
+
+		//top face
+		1,5,6,
+		1,6,2,
+
+		//bottom face
+		4,0,3,
+		4,3,7
+	};
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * 36;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = indices;
+	HR(m_pd3dDevice->CreateBuffer(&ibd, &iinitData, &mBoxIB));
 }
--- common/D3DUtil.h
+++ common/D3DUtil.h
@@ -28,4 +28,17 @@
 //方便删除COM objects
 #define ReleaseCOM(x) { if(x) { x->Release(); x = 0; }}//指针释放后 要将其值赋空值
 
+namespace Colors
+{
+	XMGLOBALCONST XMVECTORF32 White   = { 1.0f,1.0f,1.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Black   = { 0.0f,0.0f,0.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Red     = { 1.0f,0.0f,0.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Green   = { 0.0f,1.0f,0.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Blue    = { 0.0f,0.0f,1.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Yellow  = { 1.0f,1.0f,0.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Cyan    = { 0.0f,1.0f,1.0f,1.0f };
+	XMGLOBALCONST XMVECTORF32 Magenta = { 1.0f,0.0f,1.0f,1.0f };
+		
+}
+
 #endif
