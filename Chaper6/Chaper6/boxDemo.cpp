dc0a083f68281bc8cca36e1a5a5d78c2b772f192
lihao <lihaohappy521@126.com>
2018-04-08 21:05:16 +0800

    add mouse move

--- Chaper6/Chaper6/boxDemo.cpp
+++ Chaper6/Chaper6/boxDemo.cpp
@@ -102,3 +102,60 @@ void Box::OnMouseDown(WPARAM btnState, int x, int y)
 
 	SetCapture(m_hMainWnd);
 }
+
+void Box::OnMouseUp(WPARAM	btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void Box::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		//Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(
+			0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(
+			0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		//Update angles based on input to orbit camera around box.
+		mThea += dx;
+		mPhi  += dy;
+		//Restrict the angle mPhi
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if((btnState & MK_RBUTTON)!=0)
+	{
+		//Make each pixel correspond to 0.005 unit in the scene.
+		float dx = 0.005f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.005f*static_cast<float>(y - mLastMousePos.y);
+
+		//Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		//Restric the radius
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void Box::CreateGeometryBuffers()
+{
+	//create vertex buffer
+	Vertex vertex[] =
+	{
+		{XMFLOAT3(-1.0f,-1.0f,-1.0f),(const float*)&Colors::White},
+		{XMFLOAT3(-1.0f,+1.0f,-1.0f),(const float*)&Colors::Black},
+		{XMFLOAT3(+1.0f,+1.0f,-1.0f),(const float*)&Colors::Red },
+		{XMFLOAT3(+1.0f,-1.0f,-1.0f),(const float*)&Colors::Green },
+		{XMFLOAT3(-1.0f,-1.0f,+1.0f),(const float*)&Colors::Blue },
+		{XMFLOAT3(-1.0f,+1.0f,+1.0f),(const float*)&Colors::Yellow },
+		{XMFLOAT3(+1.0f,+1.0f,+1.0f),(const float*)&Colors::Cyan },
+		{XMFLOAT3(+1.0f,-1.0f,+1.0f),(const float*)&Colors::Magenta},
+	};
+
+	D3D11_BUFFER_DESC vbd;
+
+}
