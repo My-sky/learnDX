5ccc3c2f8964a9705b48f0c6628b8b03bea58f4b
lihao <lihaohappy521@126.com>
2018-07-11 12:45:20 +0800

    add chapter 10

---
+++ Chapter10/Stenciling/RenderStates.cpp
@@ -0,0 +1,62 @@
+#include"RenderStates.h"
+
+ID3D11RasterizerState*	RenderStates::pWireframeRS = 0;
+ID3D11RasterizerState*	RenderStates::pNoCullRS = 0;
+
+ID3D11BlendState*		RenderStates::pAlphaToCoverageBS = 0;
+ID3D11BlendState*		RenderStates::pTransparentBS = 0;
+
+void RenderStates::InitAll(ID3D11Device* device)
+{
+	//WireframeRS
+	D3D11_RASTERIZER_DESC wireframeDesc;
+	ZeroMemory(&wireframeDesc, sizeof(D3D11_RASTERIZER_DESC));
+	wireframeDesc.FillMode = D3D11_FILL_WIREFRAME;
+	wireframeDesc.CullMode = D3D11_CULL_BACK;
+	wireframeDesc.FrontCounterClockwise = false;
+	wireframeDesc.DepthClipEnable = true;
+
+	HR(device->CreateRasterizerState(&wireframeDesc, &pWireframeRS));
+
+	//NoCullRS
+	D3D11_RASTERIZER_DESC noCullDesc;
+	ZeroMemory(&noCullDesc, sizeof(D3D11_RASTERIZER_DESC));
+	noCullDesc.FillMode = D3D11_FILL_SOLID;
+	noCullDesc.CullMode = D3D11_CULL_NONE;
+	noCullDesc.FrontCounterClockwise = false;
+	noCullDesc.DepthClipEnable = true;
+
+	HR(device->CreateRasterizerState(&noCullDesc, &pNoCullRS));
+
+	//AlphaToCoverageBS
+	D3D11_BLEND_DESC alphaToCoverageBS = { 0 };
+	alphaToCoverageBS.AlphaToCoverageEnable = true;
+	alphaToCoverageBS.IndependentBlendEnable = false;
+	alphaToCoverageBS.RenderTarget[0].BlendEnable = false;
+	alphaToCoverageBS.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	HR(device->CreateBlendState(&alphaToCoverageBS, &pAlphaToCoverageBS));
+
+	//TransparentBS
+	D3D11_BLEND_DESC transparentBS = { 0 };
+	transparentBS.AlphaToCoverageEnable = false;
+	transparentBS.IndependentBlendEnable = false;
+
+	transparentBS.RenderTarget[0].BlendEnable = true;
+	transparentBS.RenderTarget[0].SrcBlend				= D3D11_BLEND_SRC_ALPHA;
+	transparentBS.RenderTarget[0].DestBlend				= D3D11_BLEND_INV_SRC_ALPHA;
+	transparentBS.RenderTarget[0].BlendOp				= D3D11_BLEND_OP_ADD;
+	transparentBS.RenderTarget[0].SrcBlendAlpha			= D3D11_BLEND_ONE;
+	transparentBS.RenderTarget[0].DestBlendAlpha		= D3D11_BLEND_ZERO;
+	transparentBS.RenderTarget[0].BlendOpAlpha			= D3D11_BLEND_OP_ADD;
+	transparentBS.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	HR(device->CreateBlendState(&transparentBS, &pTransparentBS));
+
+}
+
+void RenderStates::DestroyAll()
+{
+	ReleaseCOM(pWireframeRS);
+	ReleaseCOM(pNoCullRS);
+	ReleaseCOM(pAlphaToCoverageBS);
+	ReleaseCOM(pTransparentBS);
+}
---
+++ Chapter10/Stenciling/StencilingDemo.h
@@ -0,0 +1,83 @@
+#ifndef BLENDINGDEMO_H
+#define BLENDINGDEMO_H
+
+#include "D3DApp.h"
+#include "d3dx11effect.h"
+#include "mathHelper.h"
+#include "MeshGenerator.h"
+#include "Waves.h"
+#include "LightHelper.h"
+
+enum RenderOptions
+{
+	Lighting = 0,
+	Textures = 1,
+	TextureAndFog = 2
+};
+
+class Blending :public D3DApp
+{
+public:
+	Blending(HINSTANCE hInstance);
+	~Blending();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	float GetHeight(float x, float z) const;
+	XMFLOAT3 GetNormal(float x, float y) const;
+	void CreateHillGeometryBuffers();//vertex index
+	void CreateWaveGeometryBuffers();//vertex index
+	void CreateBoxGeometryBuffers();
+
+private:
+	ID3D11Buffer* pHillVB;
+	ID3D11Buffer* pHillIB;
+
+	ID3D11Buffer* pWavesVB;
+	ID3D11Buffer* pWavesIB;
+
+	ID3D11Buffer* pBoxVB;
+	ID3D11Buffer* pBoxIB;
+
+	ID3D11ShaderResourceView* pGrassMapSRV;
+	ID3D11ShaderResourceView* pWaveMapSRV;
+	ID3D11ShaderResourceView* pBoxMapSRV;
+
+	Waves mWaves;
+
+	DirectionalLight mDirLights[3];
+	Material mHillMat;
+	Material mWavesMat;
+	Material mBoxMat;
+
+	UINT mGridIndexCount;
+	XMFLOAT2 mWaveTexOffset;
+
+	XMFLOAT4X4 mGrassTexTransform;
+	XMFLOAT4X4 mWaveTexTransform;
+	XMFLOAT4X4 mHillWorld;
+	XMFLOAT4X4 mWaveWorld;
+	XMFLOAT4X4 mBoxWorld;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProject;
+
+	XMFLOAT3 mEyePosW;
+
+	RenderOptions mRenderOptions;
+
+	float mThea;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+#endif
